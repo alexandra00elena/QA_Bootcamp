7d77cfc0d195680f40f7fccbf9e2c5e1848c505a
Alexandra-Elena <alexandraelenapribagu@gmail.com>
2024-10-03 21:57:21 +0300

    Added SumOfNumbers module(ex 1)

---
+++ SumOfNumbers.java
@@ -0,0 +1,17 @@
+/// Calculate the sum of the first 100 numbers higher than 0. Name the class (and java file) SumOfNumbers.
+
+public class SumOfNumbers {
+
+    public static void main(String[] args) {
+
+        int sum = 0;
+        for (int i = 1; i <= 100; i++)
+        {
+            sum = sum + i;
+        }
+
+        System.out.println("Sum of the first 100 numbers higher than 0 is: " + sum);
+
+    }
+
+}
