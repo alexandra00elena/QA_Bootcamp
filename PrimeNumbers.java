4fdd6f1fcea536a3d6d08746f7eab8aae9612289
Alexandra-Elena <alexandraelenapribagu@gmail.com>
2024-10-03 21:52:46 +0300

    Added PrimeNumbers module

---
+++ PrimeNumbers.java
@@ -0,0 +1,24 @@
+/// Display all the prime numbers lower than 1 000 000. Name the class (and java file) PrimeNumbers
+
+public class PrimeNumbers {
+
+    public static void main(String[] args) {
+        for (int i = 2; i <= 1000000; i++)
+        {
+            boolean isPrime = true;
+            if (i > 2 && i % 2 == 0)
+                isPrime = false;
+            else {
+                for (int j = 3; j < i; j+=2) {
+                    if (i % j == 0) {
+                        isPrime = false;
+                    }
+                }
+            }
+
+            if (isPrime)
+                System.out.println("Number " + i + " is prime");
+        }
+    }
+
+}
