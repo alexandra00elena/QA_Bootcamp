098b392d4c0caa81e6a35e99ab8c567aca9671bf
Alexandra-Elena <alexandraelenapribagu@gmail.com>
2024-10-08 21:25:07 +0300

    Modified LeapYear method

--- LeapYear.java
+++ LeapYear.java
@@ -1,7 +1,7 @@
-/// Display the number of days in month February from a year between 1900-2016 that is read from keyboard(LE: parametrs)
+/// Display the number of days in month February from a year between 1900-2016 that is read from keyboard(LE: from arguments)
 ///        HINT:    A year is a leap year if:
 ///                     -Is divisible by 4 but not by 100
-///                     -Is divisible by 100 but not by 400
+///                     -Is divisible by 400
 /// Name the class (and java file) LeapYear.
 
 /// Alexandra-Elena Pribagu
@@ -13,15 +13,15 @@ public class LeapYear {
 
         int year = Integer.parseInt(args[0]);
 
-        if (year % 4 == 0 && year % 100 != 0) {
-            System.out.println(year + " is  a Leap Year");
-        }
-        else if (year % 100 == 0 && year % 400 != 0) {
-            System.out.println(year + " is a Leap Year");
+        if (year >= 1900 && year <= 2016) {
+            if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
+                System.out.println(year + " is a Leap Year");
+            } else {
+                System.out.println(year + " is not a Leap Year");
+            }
         }
         else {
-            System.out.println(year + " is not a Leap Year");
+            System.out.println(year + " is not in range 1900 - 2016!");
         }
-
     }
 }
